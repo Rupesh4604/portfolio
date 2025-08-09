a115a467560734a12d74b9b77f51a58098f10955
Rupesh <rupesh32003@gmail.com>
2025-08-09 14:41:19 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: './',
+  plugins: [react(), tailwindcss()],
 })
